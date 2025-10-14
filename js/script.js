5fca20706a8472df3c9b488e617e4ce6bcbd78c5
Oska <oskamattheus@gmail.com>
2025-10-13 21:06:08 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log("Olá")
+
+let nomeCompleto = "Oska"
+let idade = 25
+let temHabilitacao = true
+let peso = null
+const altura = 1.80
+
+console.log(altura)
+
+let carroCor = "Azul"
+let carroAno = 2018
+let carroModelo = "Gol 98"
+let carroMotor = 1.6
+
+let carro = {
+    cor: "Azul",
+    ano: 2018,
+    modelo: "Gol 98",
+    motor: 1.6
+}
+
+console.log(carro)
